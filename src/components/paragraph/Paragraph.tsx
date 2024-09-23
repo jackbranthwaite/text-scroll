0c6dc0edd365665b04906aad981c57626576fe36
Jack Branthwaite <jack@clicksuite.co.nz>
2024-09-24 08:31:27 +1200

    Basic scroll in opacity

--- src/components/paragraph/Paragraph.tsx
+++ src/components/paragraph/Paragraph.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useRef } from 'react'
 import s from './Paragraph.module.scss'
-import { useScroll } from 'framer-motion'
+import { useMotionValueEvent, useScroll, motion } from 'framer-motion'
 
 const content =
   'It is a long established fact that the reader will be distracted by the readable content of a page when looking at its layout'
@@ -10,16 +10,16 @@ export const Paragraph = () => {
   const element = useRef<HTMLParagraphElement | null>(null)
   const { scrollYProgress } = useScroll({
     target: element,
-    offset: ['start', 'end', 'start', 'start']
+    offset: ['start 0.9', 'start 0.25']
   })
 
-  useEffect(() => {
-    scrollYProgress.on('change', (e) => console.log(e))
-  }, [])
-
   return (
-    <p className={s.paragraph} ref={element}>
+    <motion.p
+      className={s.paragraph}
+      ref={element}
+      style={{ opacity: scrollYProgress }}
+    >
       {content}
-    </p>
+    </motion.p>
   )
 }
